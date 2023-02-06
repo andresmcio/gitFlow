2da43fdd6c14bfeb77108b8710a0099cd9e5c0a5
Andrés Ibarra Guerrero <andresmcio@Andress-MacBook-Pro.local>
2023-02-06 02:23:13 -0500

    #11

--- src/modules/classes.py
+++ src/modules/classes.py
@@ -1,7 +1,8 @@
 class A:
-    def __init__(self, foo, bar):
+    def __init__(self, foo, bar, lorem):
         self.foo = foo
         self.bar = bar
+        self.lorem = lorem
 
 class B:
     def __init__(self):
